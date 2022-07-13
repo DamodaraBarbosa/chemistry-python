ed0a02d45e6aea459acbb6ecc916961eab8510b2
DamodaraBarbosa <damodarabarbosa@gmail.com>
2022-07-13 18:31:24 -0300

    Implementação completa da nomenclatura de óxidos, restando peróxidos e superóxidos

--- cirpytest.py
+++
@@ -1,4 +0,0 @@
-import cirpy
-
-c = cirpy.resolve('C2H4', 'iupac_name')
-print(c)
--- formulaquim.py
+++ formulaquim.py
@@ -1,7 +1,7 @@
 import funções
+import nomenclatura
 from mendeleev import element
 from googletrans import Translator
-from chemspipy import ChemSpider 
 
 formula_mostrada = list()
 formula_dados = list()
@@ -202,7 +202,7 @@ while True:
         elif escolha == 5:
             if 'C' not in formula:
                 funções.cabeçalhoop5inorg(''.join(formula_mostrada[0:]))
-                funções.nomeclaturainorg(formula_inorg, ''.join(formula_mostrada[0:]))
+                nomenclatura.nomenclaturainorg(formula_inorg, ''.join(formula_mostrada[0:]))
             else:
                 index_carbono = list()
                 formula = list(formula)
@@ -215,14 +215,14 @@ while True:
                         formula = formula[formula.index('C'):]
                 if formula in carbono_inorg:
                     funções.cabeçalhoop5inorg(''.join(formula_mostrada[0:]))
-                    funções.nomeclaturainorg(formula_inorg, ''.join(formula_mostrada[0:]))
+                    nomenclatura.nomenclaturainorg(formula_inorg, ''.join(formula_mostrada[0:]))
                 elif grupo_parêntese != []:
                     if grupo_parêntese in carbono_inorg:
                         funções.cabeçalhoop5inorg(''.join(formula_mostrada[0:]))
                         for i, v in enumerate(formula_inorg):
                             if str(v).isalpha() == True and formula_inorg[i+1] == '(':
                                 formula_inorg.insert(i+1,'1')
-                        funções.nomeclaturainorg(formula_inorg, ''.join(formula_mostrada[0:]))
+                        nomenclatura.nomenclaturainorg(formula_inorg, ''.join(formula_mostrada[0:]))
                 else:
                     print('Espécie química orgânica.')
                     for i, v in enumerate(formula_mostrada):
