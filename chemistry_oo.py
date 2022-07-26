a64c59aa8aa37c7da167e876851790229b728940
DamodaraBarbosa <damodarabarbosa@gmail.com>
2022-07-25 23:57:48 -0300

    Projeto química oo em andamento

--- chemistry_oo.py
+++ chemistry_oo.py
@@ -1,7 +1,12 @@
-from class_chemistry import Especie
+from class_chemistry import Conjunto, Especie, Ion
 
-subs = Especie('Água', 'H2O')
+sub1 = Especie('Água', 'H2O')
+sub2 = Ion('Sulfato', 'SO4-2')
+sub3 = Ion('Cálcio', 'Ca+2')
+sub4 = Ion('Permanganato', 'MnO4-')
+sub5 = Ion('Chumbo (IV)', 'Pb+4')
 
-subs.formula_sub()
-print(subs.mostrar_dados())
+ions = [sub2, sub3, sub4, sub5]
+conjunto_esp = Conjunto(ions)
 
+print(conjunto_esp.max_min_carga())
--- class_chemistry.py
+++ class_chemistry.py
@@ -13,8 +13,14 @@ class Especie:
             formula_sub.append(caractere)
         for index, caractere in enumerate(formula_sub):
             sub = str.maketrans('0123456789', '₀₁₂₃₄₅₆₇₈₉')
-            if str(caractere).isdigit() == True:
+            sob = str.maketrans('0123456789+-', '⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻')
+            if str(caractere).isdigit() == True and index != len(formula_sub)-1:
                 formula_sub[index] = str(caractere).translate(sub)
+            if str(caractere) == '-' and str(formula_sub[index+1]).isdigit() == True or str(caractere) == '+' and str(formula_sub[index+1]).isdigit() == True:
+                formula_sub[index] = str(caractere).translate(sob)
+                formula_sub[index+1] = str(formula_sub[index+1]).translate(sob)
+            if formula_sub[len(formula_sub)-1] == '+' or formula_sub[len(formula_sub)-1] == '-':
+                formula_sub.append('1')
         self.formula = ''.join(formula_sub)
     
     def mostrar_dados(self):
@@ -24,8 +30,47 @@ class Especie:
     def nome(self):
         return self._nome
     
-class Ion(Especie):
-    def __init__(self, nome, formula, carga):
+class Ion(Especie, list):
+    def __init__(self, nome, formula):
         super().__init__(nome, formula)
-        self.carga = carga
+        self.carga = ''
+    
+    def carga_ion(self):
+        carga_ion = list()
+
+        for index, caractere in enumerate(self.formula):
+            if index == len(self.formula)-2 or index == len(self.formula)-1:
+                carga_ion.append(caractere)
+        for index, caractere in enumerate(carga_ion):
+            num_normal = str.maketrans('⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻', '0123456789+-')
+            carga_ion[index] = str(caractere).translate(num_normal)
+        self.carga = ''.join(carga_ion)
+
+class Conjunto:
+    def __init__(self, especies):
+        self._especies = especies
+    
+    def __getitem__(self, item):
+        return self._especies[item]
+
+    def max_min_carga(self):
+        maior = 0
+        menor = 0
+
+        for index, especie in enumerate(self._especies):
+            especie.formula_sub()
+            especie.carga_ion()
+            especie.carga = int(especie.carga)
+            print(especie[0])
+        #     if index == 0:
+        #         maior = menor = especie[0].carga
+        #     else:
+        #         if especie[index].carga > maior:
+        #             maior = especie[index].carga
+        #         if especie[index].carga < menor:
+        #             menor = especie[index].carga
+        # print(maior, '- maior')
+        # print(menor, '- menor')
+
+
      
